ff1be7018ad794f534f229798abe78b16178588b
herbaderm <eherbaderm@gmail.com>
2025-02-20 13:50:19 +0300

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     # Ana sayfalar
-    path('', views.home, name='home'),
+    path('', views.login_view, name='login'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     
